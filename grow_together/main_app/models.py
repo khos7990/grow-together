e9f82b3b33d3722e6b02f2f564996e6a13a6b2b3
Nayab Kara <nayab.kara@gmail.com>
2022-05-20 17:03:06 -0700

    models now with user plant

--- grow_together/main_app/models.py
+++ grow_together/main_app/models.py
@@ -18,3 +18,20 @@ class Plant(models.Model):
         return self.water_use
         return self.light
         return self.maintenance
+
+class UserPlant(models.Model):
+   custom_name= models.CharField(max_length=100)
+   created_at = models.DateTimeField
+   image= models.ImageField
+   plant=models.CharField(max_length=100)
+   plant_match= models.ManyToManyField(Plant)
+   User.id= models.ForeignKey(User, on_delete=models.CASCADE)
+   Plant.id = models.ForeignKey(Photo, )
+
+   def __str__(self):
+       return self.custom_name
+       return self.created_at
+       return self.image
+       return self.plant
+       return self.plant_match
+
