375a3904120a4e32b8c4e0097f1530152d84c02d
ale lord <j.alexlord@gmail.com>
2022-05-22 16:44:28 -0500

    add style

--- grow_together/grow_together/settings.py
+++ grow_together/grow_together/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_browser_reload',
-    'mathfilters'
+    # 'mathfilters'
 ]
 
 TAILWIND_APP_NAME = 'theme'
