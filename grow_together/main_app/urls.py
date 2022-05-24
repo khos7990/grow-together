23cfc65a5dea5951b698b2d4927ba3eb427c051e
ale lord <j.alexlord@gmail.com>
2022-05-24 12:46:56 -0500

    add comma to urls path

--- grow_together/main_app/urls.py
+++ grow_together/main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('api/<int:user_id>/add_photo/', views.test, name='add_photo'),
     path('accounts/signup/', views.signup, name='signup'),
     path('match/', views.match, name='match'),
-    path('matches/', views.matches, name='matches')
+    path('matches/', views.matches, name='matches'),
     path('match/<int:user_id>/', views.upload, name='upload'),
     path('myplants/<int:user_id>/', views.myplants, name='myplants'),
 ]
