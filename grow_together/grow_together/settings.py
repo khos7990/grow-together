a2e0dc409fe6b2ddd1b8a06a5a3acc45c0d2cc46
ale lord <j.alexlord@gmail.com>
2022-05-25 18:54:13 -0500

    login redirect

--- grow_together/grow_together/settings.py
+++ grow_together/grow_together/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Connect phone to localhost
 ALLOWED_HOSTS = ['192.168.1.2', 'localhost', '127.0.0.1']
