467d66c04d7c8d85acfcc298862c47a99d0b6ab5
ale lord <j.alexlord@gmail.com>
2022-05-26 10:27:02 -0500

    comment-out reload tailwind app in settings.py INSTALLED APPS

--- grow_together/settings.py
+++ grow_together/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_browser_reload',
+    # 'django_browser_reload',
     'tailwind',
     'widget_tweaks'
 ]
